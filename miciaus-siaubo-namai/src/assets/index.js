da28857108c85d80efb364d7f72914986c1ebcc4
Edvinas11 <edvinasty@gmail.com>
2023-10-23 20:49:47 +0300

    Initial setup

---
+++ miciaus-siaubo-namai/src/assets/index.js
@@ -0,0 +1,15 @@
+import zaliLogo from "./zali-zali.png";
+import gaidelisLogo from "./gaidelis.png";
+import catLogo from "./cat-face.png";
+import cultLogo from "./cult.png";
+import catLogoNoTitle from "./cat-no-title.png";
+import catLogoBgNoTitle from "./cat-bg-no-title.png";
+
+export {
+    zaliLogo,
+    gaidelisLogo,
+    catLogo,
+    cultLogo,
+    catLogoNoTitle,
+    catLogoBgNoTitle,
+};
---
+++ miciaus-siaubo-namai/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      }
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "769px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+}
+
